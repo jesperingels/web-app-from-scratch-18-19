a1d2181fd69d23bae614ed60ca560275d7162a75
jesperingels <jesper.ingels@gmail.com>
2019-02-04 16:36:55 +0100

    Change API to Star Wars

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,17 +1,20 @@
 let request = new XMLHttpRequest();
-let requestURL = "https://api.chucknorris.io/jokes/random";
+// let requestURL = "https://api.chucknorris.io/jokes/random";
+
+let requestURL = "https://swapi.co/api/people";
 
 // Laad een API in met veel data, Maak hier een overzicht van waarin je kunt doorklikken.
-let requestURL = "";
 
 request.onreadystatechange = function() {
     let object = JSON.parse(request.response);
     if (this.readyState === 4 && this.status === 200) {
         displayData();
+
+        console.log(request.response);
     }
 
     function displayData() {
-        document.querySelector('.joke p').innerHTML = object.value;
+        document.querySelector('.joke p').innerHTML = object.count;
     }
 
 };
