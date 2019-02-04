507086087c3f7b8202aa6aa5225966e248e409a8
jesperingels <jesper.ingels@gmail.com>
2019-02-04 14:26:29 +0100

    Show API Data in document

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,1 +1,24 @@
+let request = new XMLHttpRequest();
+let requestURL = "https://api.chucknorris.io/jokes/random";
+
+request.onreadystatechange = function() {
+    let object = JSON.parse(request.response);
+    if (this.readyState === 4 && this.status === 200) {
+        displayData();
+    }
+
+    function displayData() {
+        document.querySelector('.joke p').innerHTML = object.value;
+    }
+
+};
+
+request.open("GET", requestURL, true);
+request.send();
+
+
+
+
+
+
 
