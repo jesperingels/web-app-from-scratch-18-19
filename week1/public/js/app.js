ebaa711d087606d4c5b97411b950419d7962443f
jesperingels <jesper.ingels@gmail.com>
2019-02-04 15:29:32 +0100

    Add comment

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,5 +1,8 @@
 let request = new XMLHttpRequest();
-let requestURL = "https://api.chucknorris.io/jokes/random";
+// let requestURL = "https://api.chucknorris.io/jokes/random";
+
+// Laad een API in met veel data, Maak hier een overzicht van waarin je kunt doorklikken.
+let requestURL = "";
 
 request.onreadystatechange = function() {
     let object = JSON.parse(request.response);
